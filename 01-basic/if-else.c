9e21b6f2d3c580895a0ff6e739d7dc32569ca5d2
Shafi Swapnil <swapnil.env@gmail.com>
2023-05-27 12:24:50 +0600

    if else basics

---
+++ 01-basic/if-else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int x, y;
+  x = 22, y = 0;
+
+  if (x > y)
+  {
+    printf("x is greater than y\n");
+  }
+  else
+  {
+    printf("x is less than y\n");
+  }
+
+  return 0;
+}
