44d3027ff5e07c38514c3cdbf4af7c2a3b936189
Shafi Swapnil <swapnil.env@gmail.com>
2023-05-27 11:49:13 +0600

    first day of course init

---
+++ 01-basic/basic.c
@@ -0,0 +1,15 @@
+// basic datatype
+// int, float, double, char
+
+// function return type
+
+// library files = stdio.h, math.h
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello world!\n");
+
+  return 0;
+}
