d01ed0e1b88a2c83effb4fcc4658ea584acf6d42
Shafi Swapnil <swapnil.env@gmail.com>
2023-06-03 13:39:29 +0600

    grade problem solve extend

--- 01-basic/grade.c
+++ 01-basic/grade.c
@@ -5,7 +5,12 @@ int main()
   float score;
   scanf("%f", &score);
 
-  if (score >= 90 && score <= 100)
+  /*
+  if (score > 100)
+  {
+    printf("Invalid score\n");
+  }
+  else if (score >= 90 && score <= 100)
   {
     printf("Grade: A\n");
   }
@@ -49,6 +54,32 @@ int main()
   {
     printf("Grade: F\n");
   }
+  */
+
+  if (score > 100 || score < 0)
+    printf("Invalid score\n");
+  else if (score >= 90)
+    printf("Grade: A\n");
+  else if (score >= 86)
+    printf("Grade: A-\n");
+  else if (score >= 82)
+    printf("Grade: B+\n");
+  else if (score >= 78)
+    printf("Grade: B\n");
+  else if (score >= 74)
+    printf("Grade: B-\n");
+  else if (score >= 70)
+    printf("Grade: C+\n");
+  else if (score >= 66)
+    printf("Grade: C\n");
+  else if (score >= 62)
+    printf("Grade: C-\n");
+  else if (score >= 58)
+    printf("Grade: D+\n");
+  else if (score >= 55)
+    printf("Grade: D\n");
+  else
+    printf("Grade: F\n");
 
   return 0;
 }
