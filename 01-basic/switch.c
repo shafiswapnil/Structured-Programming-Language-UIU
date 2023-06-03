a568b2ebc7b2684bcdef1a2a225bf92724302307
Shafi Swapnil <swapnil.env@gmail.com>
2023-06-03 11:52:56 +0600

    switch statement

---
+++ 01-basic/switch.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+  int x;
+  scanf("%d", &x);
+
+  switch (x)
+  {
+  case 0:
+    printf("zero\n");
+    break;
+
+  case 1:
+    printf("one\n");
+    break;
+
+  case 2:
+    printf("two\n");
+    break;
+
+  case 3:
+    printf("three\n");
+    break;
+
+  default:
+    printf("Invalid number\n");
+  }
+}
