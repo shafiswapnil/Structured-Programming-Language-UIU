e62c37971c1b54b49e2912fd5b72dd9f4cb1b3fa
Shafi Swapnil <swapnil.env@gmail.com>
2023-06-10 12:57:14 +0600

    series 10 solve

---
+++ 01-classwork/series10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  int input, sum = 0;
+  scanf("%d", &input);
+
+  for (int i = 1; i <= input; i++)
+  {
+    if (i % 2 == 1) // odd
+    {
+      sum = sum + i; // odd ashle sum e jog hocche
+    }
+    else // even
+    {
+      sum = sum - i; // even number sum variable e biyog hocche
+    }
+  }
+
+  printf("Result: %d\n", sum);
+}
