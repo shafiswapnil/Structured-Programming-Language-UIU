ec7cbcb0e8cb74be542cd345f00afd5f6d28c681
Shafi Swapnil <swapnil.env@gmail.com>
2023-06-10 12:41:48 +0600

    series problem 11 solve

--- 01-classwork/series11.c
+++ 01-classwork/series11.c
@@ -2,4 +2,15 @@
 
 int main()
 {
+  int input, sum = 0;
+  scanf("%d", &input);
+
+  for (int i = 1; i <= input; i++)
+  {
+    sum = sum + ((i * i) * (i + 1));
+  }
+
+  printf("Result: %d\n", sum);
+
+  return 0;
 }
